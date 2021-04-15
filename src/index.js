f9aae7faec6c2f0cf11bac9345d8e55c1e079eec
Asti <asti-maera@live.nmit.ac.nz>
2021-04-15 13:02:40 +1200

    task three with dot notation

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@
 Create a function that tallys the sum of an objects values
 14/04
 Asti Maera */
-function likeTally(obj) {
-    let arr = [];
-    for (var prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-            arr.push((obj[prop]))
-        }
-    }
-    let total = (`${arr[0] - arr[1]}`)
-    return total
-}
-console.log(likeTally({ likes: 12, dislikes: 15, }))
+
+function likeTally(obj) { //declare function
+    for (var prop in obj) { //for variable found in obj parameter
+        if (obj.hasOwnProperty(prop)) //if the object has properties
+            return obj.likes - obj.dislikes; //return the object key likes value minus the object key dislikes value.
+    };
+}; console.log(likeTally({ likes: 12, dislikes: 15, })) //print to console the calculated object value (-3)
+// function likeTally(obj) {
+//     let arr = [];
+//     for (var prop in obj) {
+//         if (obj.hasOwnProperty(prop)) {
+//             arr.push((obj[prop]))
+//         }
+//     }
+//     let total = (`${arr[0] - arr[1]}`)
+//     return total
+// }
+//console.log(likeTally({ likes: 12, dislikes: 15, }))
 // const likeSum = obj => Object.values(obj).reduce((a, b) => a - b);
 // console.log(likeSum({likes: 12, dislikes : 4}))
