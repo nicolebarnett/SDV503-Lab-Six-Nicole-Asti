c91fa56b243f21374c589e0b572d2c9a3837bbd5
Heccin <Ree>
2021-04-15 13:30:27 +1200

    finished func1!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 //TODO 
 //Create function to  returns the total of all odd numbers up to and including the given argument
 
-function sumOdd(arr){
-    let sum = 0;
-    for(let i = 5; i< 429; i++) {
-    if(i % 2 !== 0) {
-    sum = sum + i;
-                }
-            }
-    return sum;
+function sumOdd(num){ //creating a function and its name is sumOdd and its parameter is num
+    let arr = [] //creating an empty array 
+    for(let i=0;i<=num;i++){ //create a for loop 
+    if(i%2 !==0){ //check if number is odd
+    arr.push(i) //if it cannot be divided by 2, push into array
+    }
+    }
+    let result = arr.reduce((a,b)=> a+b) // using reduce method to get sum of array of number 
+    return result //return
     }
 
-console.log()
+console.log(sumOdd(13)) 
+//print!
